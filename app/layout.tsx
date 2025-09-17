2f3c08cfcb21eb398e23d00f963b57c09dc58f5b
Kyle Par <shuttle876@gmail.com>
2025-09-16 23:28:02 -0400

    Fixed bugs

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "./component/navbar";
 import { Analytics } from "@vercel/analytics/next"
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 export const metadata: Metadata = {
   title: "Patient Directory",
@@ -21,6 +22,7 @@ export default function RootLayout({
         <Navbar />
         {/* <div className="min-h-screen bg-muted/20"> */}
           <section className="max-w-7xl mx-auto p-6">{children}</section>
+          <GoogleAnalytics gaId="G-FJX5J4G4ZW" />
         {/* </div> */}
       </body>
     </html>
