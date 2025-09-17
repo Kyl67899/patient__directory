721a881bf5ebc9141f331e02d1b5b2f8b1eb557c
Kyle Par <shuttle876@gmail.com>
2025-09-16 23:22:24 -0400

    Fixed bugs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "./component/navbar";
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: "Patient Directory",
@@ -16,6 +17,7 @@ export default function RootLayout({
     <html lang="en">
       <body
       >
+        <Analytics />
         <Navbar />
         {/* <div className="min-h-screen bg-muted/20"> */}
           <section className="max-w-7xl mx-auto p-6">{children}</section>
